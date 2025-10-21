741efcdbb2165945bbf755b8370815ca4b7a9fe4
sammie004 <123166502+sammie004@users.noreply.github.com>
2025-10-21 13:36:44 +0100

    script

--- scripts.js
+++ scripts.js
@@ -23,7 +23,7 @@ const response = await fetch("http://localhost:3000/check/validate", {
   headers: {
     "Content-Type": "application/json",
   },
-  body: JSON.stringify({ accountNumber:accNumber }),
+  body: JSON.stringify({ accountNumber: accNumber }),
 });
     if (!response.ok) throw new Error("Network response was not okay");
 
